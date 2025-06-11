e87c7d729161a44673c2aa68a7fbb3e168516834
Andrew Gosali <andrewgosali@gmail.com>
2025-06-11 13:24:17 +0700

    MDL-82996 core_form: enable hideIf on header element

--- customfield/field/number/classes/local/numberproviders/nofactivities.php
+++ customfield/field/number/classes/local/numberproviders/nofactivities.php
@@ -70,8 +70,6 @@ class nofactivities extends provider_base {
         $mform->addElement('autocomplete', 'configdata[activitytypes]', $valuelabel, $options, ['multiple' => true]);
         $mform->hideIf('configdata[activitytypes]', 'configdata[fieldtype]', 'ne', get_class($this));
         $mform->hideIf('configdata[decimalplaces]', 'configdata[fieldtype]', 'eq', get_class($this));
-        $mform->hideIf('configdata[display]', 'configdata[fieldtype]', 'eq', get_class($this));
-        $mform->hideIf('str_display_format', 'configdata[fieldtype]', 'eq', get_class($this));
         $mform->hideIf('configdata[defaultvalue]', 'configdata[fieldtype]', 'eq', get_class($this));
         $mform->hideIf('configdata[minimumvalue]', 'configdata[fieldtype]', 'eq', get_class($this));
         $mform->hideIf('configdata[maximumvalue]', 'configdata[fieldtype]', 'eq', get_class($this));
